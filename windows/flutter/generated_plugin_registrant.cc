3b30f7e79a387d1ec44bb3d527804817becfd825
Éricles Dos Santos <67772327+Ericles-Porty@users.noreply.github.com>
2025-05-07 01:57:45 -0300

    refactor: improve code formatting and consistency across various files

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -11,13 +11,13 @@
 #include <sqlite3_flutter_libs/sqlite3_flutter_libs_plugin.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 
-void RegisterPlugins(flutter::PluginRegistry* registry) {
-  PermissionHandlerWindowsPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
-  SharePlusWindowsPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("SharePlusWindowsPluginCApi"));
-  Sqlite3FlutterLibsPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("Sqlite3FlutterLibsPlugin"));
-  UrlLauncherWindowsRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("UrlLauncherWindows"));
+void RegisterPlugins(flutter::PluginRegistry *registry) {
+    PermissionHandlerWindowsPluginRegisterWithRegistrar(
+            registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
+    SharePlusWindowsPluginCApiRegisterWithRegistrar(
+            registry->GetRegistrarForPlugin("SharePlusWindowsPluginCApi"));
+    Sqlite3FlutterLibsPluginRegisterWithRegistrar(
+            registry->GetRegistrarForPlugin("Sqlite3FlutterLibsPlugin"));
+    UrlLauncherWindowsRegisterWithRegistrar(
+            registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
